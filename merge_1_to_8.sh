e56cc7de3612c6ab49f76f36a395090c3ee267ca
dung nguyen <dungnt109@gmail.com>
2025-04-10 14:05:27 +0800

    adding merge_1_to_8.sh

---
+++ merge_1_to_8.sh
@@ -0,0 +1,25 @@
+echo "Create output folder."
+mkdir output
+
+
+for prefix in $(ls *R1* | awk -F- '{print $1"-"$2"-"$3}' | uniq) ; do
+
+  echo Merging...
+      
+  ls *$prefix*R1.fastq.gz | tr " " "\n"
+
+  ls *$prefix*R1.fastq.gz | xargs cat > output/${prefix}_R1.fastq.gz
+
+  echo Saved to output/${prefix}_R1.fastq.gz
+
+  echo " "
+
+  echo Merging...
+
+  ls *$prefix*R2.fastq.gz | tr " " "\n"
+
+  ls *$prefix*R2.fastq.gz | xargs cat > output/${prefix}_R2.fastq.gz
+
+  echo Saved to output/${prefix}_R2.fastq.gz
+
+done
