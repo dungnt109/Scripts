7facfdcad6798bc95f6023e1162cc37d14dc57f3
dung nguyen <dungnt109@gmail.com>
2025-04-10 15:40:42 +0800

    some changes

--- merge_replicates.sh
+++ merge_replicates.sh
@@ -6,20 +6,22 @@ for prefix in $(ls *R1* | awk -F- '{print $1"-"$2"-"$3}' | uniq) ; do
 
   echo Merging...
       
-  ls *$prefix*R1.fastq.gz | tr " " "\n"
+  ls *$prefix*R1*fastq.gz | tr " " "\n"
 
-  ls *$prefix*R1.fastq.gz | xargs cat > output/${prefix}_R1.fastq.gz
+  ls *$prefix*R1*fastq.gz | xargs cat > output/${prefix}_R1.fastq.gz
 
   echo Saved to output/${prefix}_R1.fastq.gz
 
-  echo " "
+  echo "----"
 
   echo Merging...
 
-  ls *$prefix*R2.fastq.gz | tr " " "\n"
+  ls *$prefix*R2*fastq.gz | tr " " "\n"
 
-  ls *$prefix*R2.fastq.gz | xargs cat > output/${prefix}_R2.fastq.gz
+  ls *$prefix*R2*fastq.gz | xargs cat > output/${prefix}_R2.fastq.gz
 
   echo Saved to output/${prefix}_R2.fastq.gz
 
 done
+
+echo "Finished"
