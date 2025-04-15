ace48451c721085ba1355150aafb4ecc3ce53156
dung nguyen <dungnt109@gmail.com>
2025-04-15 09:59:17 +0800

    first commit of TMS screenshot

---
+++ TMS screenshot/screenshot.bat
@@ -0,0 +1,1 @@
+pythonw.exe C:\Users\HaemOnco\Desktop\screenshot.py
---
+++ TMS screenshot/screenshot.py
@@ -0,0 +1,38 @@
+import schedule
+import time
+import datetime
+from PIL import ImageGrab
+import os
+
+
+print("Take screenhots daily at 9 AM...")
+print("Don't close this window.")
+
+def take_screenshot():
+
+    print("take screenshot...")
+
+    date_string = datetime.datetime.now().strftime("%d-%m-%Y_%H_%M_%S")
+    folder = datetime.datetime.now().strftime("%d-%m-%Y")
+
+    if not os.path.isdir("C:\\Users\\HaemOnco\\Documents\\screenshots\\"+ folder):
+        os.mkdir("C:\\Users\\HaemOnco\\Documents\\screenshots\\"+ folder) 
+    save_path = "C:\\Users\\HaemOnco\\Documents\\screenshots\\"+ folder + "\\" + date_string + "_DN.png"
+
+    print(save_path)
+
+    snapshot = ImageGrab.grab()
+
+    snapshot.save(save_path)
+
+for hour in range(0, 24): 
+    schedule.every().day.at(f"{hour:02d}:00").do(take_screenshot)
+
+
+while True:
+    
+    schedule.run_pending()
+    time.sleep(60) # wait one minute
+
+#take_screenshot()
+    
