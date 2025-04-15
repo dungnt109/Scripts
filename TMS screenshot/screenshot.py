f80cd45611012c66b78ad506073516befb0d64ba
dung nguyen <dungnt109@gmail.com>
2025-04-15 15:29:23 +0800

    some changes

--- TMS screenshot/screenshot.py
+++ TMS screenshot/screenshot.py
@@ -1,42 +1,64 @@
 import schedule
 import time
 import datetime
-from PIL import ImageGrab
+import pyscreenshot as ImageGrab
+#from PIL import ImageGrab
 import os
+from PIL import Image
 
+print("take screenthots...")
 
-print("Take screenhots daily at 9 AM...")
-print("Don't close this window.")
+path = "/home/dungnt/Downloads/tms/" 
+#path = "C:\\Users\\HaemOnco\\Documents\\screenshots\\"
 
-path = "C:\\Users\\HaemOnco\\Documents\\screenshots\\"
-
-separator = "\\"
+separator = "/"  
+#separator = "\\"
 
 def take_screenshot():
 
-    print("take screenshot...")
+	
 
-    date_string = datetime.datetime.now().strftime("%d-%m-%Y_%H_%M_%S")
-    folder = datetime.datetime.now().strftime("%d-%m-%Y")
+	current_time = datetime.datetime.now()
 
-    if not os.path.isdir( path + folder):
-        os.mkdir(path + folder) 
-    save_path = path + folder + separator + date_string + "_DN.png"
+	year = str(current_time.year) 
 
-    print(save_path)
+	month = current_time.strftime("%m")
 
-    snapshot = ImageGrab.grab()
+	date_string = current_time.strftime("%d-%m-%Y_%H_%M_00")
+	folder = current_time.strftime("%d-%m-%Y")
 
-    snapshot.save(save_path)
+	if not os.path.isdir( path + year + separator + month + separator + folder ):
 
-for hour in range(0, 24): 
-    schedule.every().day.at(f"{hour:02d}:00").do(take_screenshot)
+		os.makedirs(path + year + separator + month + separator + folder) 
+
+	save_path = path + year + separator + month + separator + folder + separator + date_string 
+
+	print(save_path)
+
+	snapshot = ImageGrab.grab()
 
 
-while True:
+	width, height = snapshot.size
+
+
+	new_size = (int(width * 3/4), int(height * 3/4))
+	resized_snapshot = snapshot.resize(new_size, Image.LANCZOS)
+
+
+	resized_snapshot.convert("RGB").save(save_path + ".jpg", "JPEG")
+	resized_snapshot.save(save_path + ".webp", "WEBP")
+
+	print("take screenshot at " + date_string)
+
+for hour in range(0, 12): 
+    #schedule.every().day.at(f"{hour:02d}:00").do(take_screenshot)
+    schedule.every().hour.at(":"+str(hour*5).zfill(2)).do(take_screenshot)
+
+
+#while True:
     
-    schedule.run_pending()
-    time.sleep(60) # wait one minute
+#    schedule.run_pending()
+#    time.sleep(60) # wait one minute
 
-#take_screenshot()
+take_screenshot()
     
